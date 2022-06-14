f135b8e271f4bb229a6d0e89e032df61aaca44ad
Serverr1 <service.prowriters@gmail.com>
2022-06-14 05:35:16 +0100

    added minting contract and reentrancy guard protection

--- auction.sol
+++ auction.sol
@@ -8,6 +8,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 
 contract AuctionNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
@@ -60,7 +61,7 @@ contract AuctionNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
 }
 
 
-contract Auctioner {
+contract Auctioner is ReentrancyGuard  {
     using Timers for Timers.Timestamp;
 
     event Start(uint indexed nftId, address owner, uint startAmount);
